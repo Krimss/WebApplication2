3f8781c90bb5ef219bb85faf1e5d872148dc133e
Роман Валвенки <roomann@outlook.com>
2020-07-24 11:04:07 +0300

    d

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult GetReport()
+        {
+            StiReport report = new StiReport();
+            report.Load(StiNetCoreHelper.MapPath(this, "reports/13contest.mrt"));
+            return StiNetCoreViewer.GetReportResult(this, report);
+        }
+        public IActionResult ViewerEvent()
+        {
+            return StiNetCoreViewer.ViewerEventResult(this);     
+        }
+    }
+        
+}
